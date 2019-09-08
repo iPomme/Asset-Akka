5e163a3fcdda0b85e75e898eca2a96b0a6945f70
Nicolas Jorand <nicolas.jorand@me.com>
2019-09-08 23:46:52 +0200

    Rename the ActorSystem to AkkaSystem, add the possibility to create child actor by message.

---
+++ Assets/Akka/ScriptObject/AkkaSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Akka.Actor;
+using Akka.Configuration;
+
+public class AkkaSystem : ScriptableObject
+{
+    private static readonly ActorSystem _system =
+        ActorSystem.Create("AkkaSystem", ConfigurationFactory.ParseString(@""));
+
+    private static AkkaSystem _inst;
+
+    public static AkkaSystem Instance
+    {
+        get
+        {
+            if (!_inst)
+            {
+                var classes = Resources.FindObjectsOfTypeAll<AkkaSystem>();
+                if (classes.Length > 0)
+                    _inst = classes.First();
+            }
+
+            if (!_inst)
+                _inst = CreateInstance<AkkaSystem>();
+            return _inst;
+        }
+    }
+
+    public ActorSystem GetActorSystem()
+    {
+        return _system;
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Destrowing the system Actor ....");
+        _system.Terminate();
+    }
+}
