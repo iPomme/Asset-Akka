c218987954b882b21b2571acbb069135b8feff66
Nicolas Jorand <nicolas.jorand@me.com>
2020-01-16 16:21:47 +0100

    stop the actor when the application stop

--- Assets/Akka/Examples/Scripts/ActorSystem.cs
+++ Assets/Akka/Examples/Scripts/ActorSystem.cs
@@ -7,21 +7,32 @@ namespace Assets.Scripts
     public class ActorSystem : MonoBehaviour
     {
         public static readonly RootContext Context = new RootContext();
+        private PID _pingerActor;
+        private PID _pongerActor;
+        private PID _uiManagerActor;
 
         private void Start()
         {
             var props = Props.FromProducer(() => new UIManagerActor());
-            var actor = Context.SpawnNamed(props, "UIManagerActor");
+            _uiManagerActor = Context.SpawnNamed(props, "UIManagerActor");
             UnityEngine.Debug.Log("PingPong Starting ....");
 //            var ret = Context.RequestAsync<object>(actor, new Hello() {Who = "Nicolas"});
             // Create the ping and the pong actor
-            var pongerActor = Context.Spawn(Props.FromProducer(() =>
-                new PingPongActor(new Messages.Ping(), new Messages.Pong(), actor)));
-            var pingerActor = Context.Spawn(Props.FromProducer(() =>
-                new PingPongActor(new Messages.Pong(), new Messages.Ping(), actor)));
+            _pongerActor = Context.Spawn(Props.FromProducer(() =>
+                new PingPongActor(new Messages.Ping(), new Messages.Pong(), _uiManagerActor)));
+            _pingerActor = Context.Spawn(Props.FromProducer(() =>
+                new PingPongActor(new Messages.Pong(), new Messages.Ping(), _uiManagerActor)));
             
-            Context.Request(pingerActor, new Messages.Pong(), pongerActor);
-            
+            Context.Request(_pingerActor, new Messages.Pong(), _pongerActor);
+
+        }
+
+
+        private void OnDestroy()
+        {
+            _pingerActor.Stop();
+            _pongerActor.Stop();
+            _uiManagerActor.Stop();
         }
     }
 }
